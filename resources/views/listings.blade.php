c46b7aac48d3d0e8b08ae59ff6308faffd364b97
starlingvibes <starlingroot@protonmail.com>
2023-04-29 13:40:22 +0100

    chore(): setting up html template

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -10,14 +10,14 @@
                     <div class="flex">
                         <img
                             class="hidden w-48 mr-6 md:block"
-                            src="images/acme.png"
+                            src="{{asset('images/no-image.png')}}"
                             alt=""
                         />
                         <div>
                             <h3 class="text-2xl">
-                                <a href="show.html">Senior Laravel Developer</a>
+                                <a href="show.html">{{$listing->title}}</a>
                             </h3>
-                            <div class="text-xl font-bold mb-4">Acme Corp</div>
+                            <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                             <ul class="flex">
                                 <li
                                     class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
@@ -41,8 +41,7 @@
                                 </li>
                             </ul>
                             <div class="text-lg mt-4">
-                                <i class="fa-solid fa-location-dot"></i> Boston,
-                                MA
+                                <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
                             </div>
                         </div>
                     </div>
