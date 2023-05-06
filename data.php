8422289d194107b6c33d128b21e2100b61d33b93
starlingvibes <starlingroot@protonmail.com>
2023-05-06 01:28:40 +0100

    feat(): creating seeders and factories

---
+++ data.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'title' => 'Senior Software Engineer',
+        'tags' => 'PHP, Laravel, Vue.js, Tailwind CSS',
+        'company' => 'Laravel Jobs',
+        'location' => 'Remote',
+        'email' => 'test@test.com',
+        'website' => 'https://laraveljobs.com',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc sapien ultricies nunc, vitae al',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Senior Software Engineer',
+        'tags' => 'PHP, Laravel, Vue.js, Tailwind CSS',
+        'company' => 'Laravel Jobs',
+        'location' => 'Remote',
+        'email' => '',
+        'website' => 'https://laraveljobs.com',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc sapien ultricies nunc, vitae al',
+    ]
+];
+?>
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +13,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(2)->create();
+
+        Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'id' => 1,
+        //     'title' => 'Senior Software Engineer',
+        //     'tags' => 'PHP, Laravel, Vue.js, Tailwind CSS',
+        //     'company' => 'Laravel Jobs',
+        //     'location' => 'Remote',
+        //     'email' => 'test@test.com',
+        //     'website' => 'https://laraveljobs.com',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc sapien ultricies nunc, vitae al',
+        // ]);
+
+        // Listing::create(
+        //     [
+        //         'id' => 2,
+        //         'title' => 'Junior Software Engineer',
+        //         'tags' => 'PHP, Laravel, Vue.js, Tailwind CSS',
+        //         'company' => 'Laravel Jobs',
+        //         'location' => 'Remote',
+        //         'email' => '',
+        //         'website' => 'https://laraveljobs.com',
+        //         'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc sapien ultricies nunc, vitae al',
+        //     ]
+        // );
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
